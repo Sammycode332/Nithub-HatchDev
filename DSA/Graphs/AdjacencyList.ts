f291efc9ac866795936964509f2bd5d81c71ea96
Sammycode332 <samueloroja123@gmail.com>
2026-09-11 17:22:48 +0100

    Add initial Graph class implementation with methods for adding vertices and edges

---
+++ DSA/Graphs/AdjacencyList.ts
@@ -0,0 +1,13 @@
+class Graph {
+    Numbers: number[] = []
+    constructor(Number: number[]){
+        this.Numbers = Number
+    }
+
+    addVertex(vertex: number){
+
+    }
+    addEdge(u: number v: number){
+        
+    }
+}
